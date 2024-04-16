b7fd1aca65b6fe2259675047a2e78ae8a8ac79a9
roodruh <161749877+roodruh@users.noreply.github.com>
2024-04-16 17:47:11 +0530

    Trying DOM Manipulation through JavaScript Will Create UI for Rock-Paper-Scissors Game

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector('ul.list');
+const li = document.createElement('li');
+
+ul.appendChild(li);
+
+li.innerText = "Deathnote";
+
+li.setAttribute('class', 'list-items');
